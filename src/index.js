308d9edcc289520cc7af94ef1b2c673718648690
nurtazim <bakytovnurtazim@gmail.com>
2022-02-08 16:42:44 +0600

    react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,67 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class Counter extends React.Component {
+
+    state = {
+        count: 0
+    }
+    onInCreate = () => {
+        this.setState((oldState) => {
+            return {
+                count: oldState.count + 1
+            }
+        })
+    }
+
+
+    render() {
+        console.log("render")
+        return (
+            <div>
+                <p>{this.state.count}</p>
+                <input type="button" onClick={this.onInCreate} value="InCase"/>
+                <input type="button" onClick={this.DeCreate} value="DeCreate"/>
+            </div>
+        )
+    }
+
+
+}
+
+class App extends React.Component {
+
+    state = {
+        showCounter: true
+    }
+
+    onToggleCounter = () => {
+        this.setState((oldState) => {
+            return {
+                showCounter: !oldState.showCounter
+            }
+        })
+    }
+
+    render() {
+        const content = this.state.showCounter ? <Counter/> : null
+        return (
+            <>
+                {content}
+                <button onClick={this.onToggleCounter}> ToggleContent</button>
+            </>
+
+        )
+    }
+}
+
+ReactDOM
+    .render(
+        <App/>,
+        document
+            .getElementById(
+                'root'
+            )
+    )
+
